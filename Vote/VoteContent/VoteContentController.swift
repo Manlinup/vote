3643cec2a23285a2ac094e81c99149e253be0944
林以达 <manlin@mcdeMacBook-Pro.local>
2017-11-15 09:25:03 +0800

    Inital commit

---
+++ Vote/VoteContent/VoteContentController.swift
@@ -0,0 +1,35 @@
+//
+//  VoteContentController.swift
+//  Vote
+//
+//  Created by 林以达 on 2017/11/14.
+//  Copyright © 2017年 林以达. All rights reserved.
+//
+
+import UIKit
+
+class VoteContentController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
