98248b1335494e10720a4447ba6cfeb3f09de589
林以达 <manlin@mcdeMacBook-Pro.local>
2017-11-16 17:59:13 +0800

    Inital commit

---
+++ PutListCell.swift
@@ -0,0 +1,24 @@
+//
+//  PutListCell.swift
+//  Vote
+//
+//  Created by 林以达 on 2017/11/16.
+//  Copyright © 2017年 林以达. All rights reserved.
+//
+
+import UIKit
+
+class PutListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
